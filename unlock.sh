07a7aa363b86ebdbacba2869a2c34a0f8d08ff27
captmicr0 <92401273+captmicr0@users.noreply.github.com>
2023-11-04 02:12:55 -0400

    Update unlock.sh

--- unlock.sh
+++ unlock.sh
@@ -5,7 +5,7 @@ cat << EOF >> /etc/rc.local
 ## make sure msr module loaded
 modprobe msr
 ## unlock
-wrmsr 0x1FC "$(printf '0x%x' "$(( 0x$(sudo rdmsr 0x1FC) & 0xFFFFFFFE ))")"
+wrmsr 0x1FC "$(printf '0x%x' "$(( 0x$(rdmsr 0x1FC) & 0xFFFFFFFE ))")"
 ## might not work without setting no_turbo to 0 even though it's already 0
 echo 0 | tee /sys/devices/system/cpu/intel_pstate/no_turbo 
 EOF
